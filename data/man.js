0624d32730f90c4818b3dd0034cd6276ea5a6051
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2022-05-01 11:29:31 +0530

    create men data

---
+++ data/man.js
@@ -0,0 +1,105 @@
+
+function newManData (i,n,p){
+    this.image=i;
+    this.name=n;
+    this.price=p;
+}
+
+let one_n1 = new newManData('https://static.zara.net/photos///2022/V/0/1/p/3918/420/439/34/w/1205/3918420439_13_1_1.jpg?ts=1650456555880','OVERSIZE TECHNICAL PARKA','8990.00')
+
+let two_n2 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','OVERSIZE TECHNICAL PARKA','8990.00')
+let two_n3 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','OVERSIZE TECHNICAL PARKA',' 8990.00')
+
+let one_n4 = new newManData('https://static.zara.net/photos///2022/V/M/2/p/0000/002/638/2/w/725/0000002638_15_2_1.jpg?ts=1650466739036','')
+
+let one_n5 = new newManData('https://static.zara.net/photos///2022/V/0/1/p/2553/417/251/35/w/1205/2553417251_15_1_1.jpg?ts=1650470774279','BALLOON FIT TROUSERS','4990.00');
+
+let two_n6 = new newManData('https://static.zara.net/photos///2022/V/M/1/p/0000/002/639/2/w/725/0000002639_15_2_1.jpg?ts=1650448653514')
+
+let one_n7 = new newManData('https://www.zara.com/in/en/balloon-fit-trousers-p02553417.html?v1=181605722&v2=2032934','BALLOON FIT TROUSERS','4990.00');
+
+let two_n8 = new newManData('https://static.zara.net/photos///2022/V/0/1/p/2553/417/251/32/w/513/2553417251_1_1_1.jpg?ts=1650528268896','BALLOON FIT TROUSERS','4990.00');
+let two_n9 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/6048/084/400/32/w/513/6048084400_1_1_1.jpg?ts=1650378281990','STRIPED TEXTURED SHIRT','2990.00');
+
+let one_n10 = new newManData('https://static.zara.net/photos///2022/V/0/1/p/3918/422/505/33/w/725/3918422505_15_2_1.jpg?ts=1650469435406','TECHNICAL PARKA WITH POUCH POCKET','6990.00');
+
+let two_n11 =new newManData('https://static.zara.net/photos///2022/V/0/2/p/3918/422/505/32/w/513/3918422505_2_1_1.jpg?ts=1650378281459','TECHNICAL PARKA WITH POUCH POCKET','6990.00');
+let two_n12 = new newManData('https://static.zara.net/photos///2022/V/0/1/p/3918/422/505/32/w/513/3918422505_1_1_1.jpg?ts=1650444837903','TECHNICAL PARKA WITH POUCH POCKET','6990.00');
+
+let one_n13 = new newManData('https://www.zara.com/in/en/technical-parka-with-pouch-pocket-p03918422.html?v1=181502589&v2=2032934','TECHNICAL PARKA WITH POUCH POCKET','6990.00');
+
+let one_n14 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/2675/370/612/32/w/590/2675370612_15_2_1.jpg?ts=1650466898785','OVERSHIRT WITH POCKETS','4990.00');
+
+
+//2.
+let lunarLavender = [];
+
+let lun1 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','OVERSIZE TECHNICAL PARKA',' 8990.00');
+let lun2 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','CARROT FIT TROUSERS','4990.00');
+let lun3 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/2675/370/612/2/w/373/2675370612_1_1_1.jpg?ts=1650443697160','OVERSHIRT WITH POCKETS',' 4990.00');
+let lun4 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/6917/442/612/2/w/373/6917442612_1_1_1.jpg?ts=1650450165287','POPLIN BERMUDA SHORTS WITH PLEATS','2990.00')
+let lun5 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/3918/422/505/2/w/373/3918422505_1_1_1.jpg?ts=1650450124432','TECHNICAL PARKA WITH POUCH POCKET','6990.00');
+let lun6 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/3918/422/505/2/w/373/3918422505_2_6_1.jpg?ts=1650450143084','TECHNICAL PARKA WITH POUCH POCKET',' 6990.00');
+let lun7 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','STRIPED TEXTURED SHIRT','2990.00');
+let lun8 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/6048/084/400/2/w/373/6048084400_2_1_1.jpg?ts=1650450125592','STRIPED TEXTURED SHIRT','2990.00');
+
+lunarLavender.push(lun1,lun2,lun3,lun4,lun5,lun6,lun7,lun8);
+
+
+//3.
+let   Blazer = [];
+let bl1 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/4541/863/710/32/w/513/4541863710_1_1_1.jpg?ts=1650382410747','CHECK BLAZER',' 8990.00');
+let bl2 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','CHECK BLAZER','8990.00');
+let bl3 = new newManData('https://static.zara.net/photos///2022/V/0/1/p/4541/863/710/33/w/725/4541863710_15_1_1.jpg?ts=1650452720105','CHECK BLAZER',' 8990.00');
+
+Blazer.push(bl1,bl2,bl3);
+
+
+//4.
+let shoe = [];
+let s1 = new newManData('https://www.zara.com/in/en/leather-brogues-p12410920.html?v1=181502569&v2=2032934','LEATHER BROGUES','7990.00');
+shoe.push(s1);
+
+
+//5.
+let mordernPrep = [];
+let mp1 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/4540/862/401/2/w/373/4540862401_1_1_1.jpg?ts=1650451327241','TEXTURED DOUBLE-BREASTED BLAZER','8990.00');
+let mp2= new newManData('https://static.zara.net/photos///2022/V/0/2/p/4489/965/500/2/w/373/4489965500_1_1_1.jpg?ts=1650450124548',`LINEN - COTTON BLEND SHIRT
+`,'2990.00')
+let mp3 = new newManData('https://static.zara.net/photos///2022/V/0/1/p/0975/113/058/2/w/373/0975113058_1_1_1.jpg?ts=1650464563352','STRIPED SHIRT','2990.00');
+let mp4 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','STRIPED T­SHIRT','1990.00');
+let mp5 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','SLIM FIT CHINO TROUSERS','3990.00');
+let mp6 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','PLEATED BERMUDA SHORTS',' 2990.00');
+let mp7 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/4489/965/250/2/w/373/4489965250_1_1_1.jpg?ts=1650464287302',`LINEN - COTTON BLEND SHIRT`,' 2990.00');
+let mp8 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/4489/965/600/2/w/373/4489965600_1_1_1.jpg?ts=1650463141814',`LINEN - COTTON BLEND SHIRT`,'2990.00');
+let mp9 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/4541/863/710/2/w/373/4541863710_1_1_1.jpg?ts=1650451313835','CHECK BLAZER','8990.00');
+let mp10 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','PLEATED BERMUDA SHORTS','2990.00');
+let mp11 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/6917/470/306/2/w/373/6917470306_1_1_1.jpg?ts=1650464286424','SLIM FIT CHINO TROUSERS',' 3990.00');
+let mp12 = new newManData('https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013','PLEATED BERMUDA SHORTS','2990.00');
+
+mordernPrep.push(mp1,mp2,mp3,mp4,mp5,mp6,mp7,mp8,mp9,mp9,mp10,mp11,mp12);
+
+
+//6.
+let tie = [];
+let tie1 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/5568/429/401/2/w/283/5568429401_6_1_1.jpg?ts=1649084990381','JACQUARD TIE WITH BASEBALL MOTIF..','2590.00');
+let tie2 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/5568/430/500/2/w/283/5568430500_6_1_1.jpg?ts=1650556576894','SURFER JACQUARD TIE','2590.00');
+let tie3 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/2253/400/401/2/w/283/2253400401_6_1_1.jpg?ts=1650616271195','JACQUARD TIE WITH INSECTS','JACQUARD TIE WITH INSECTS','2590.00');
+let tie4 = new newManData('https://static.zara.net/photos///2022/V/0/2/p/7347/482/407/2/w/283/7347482407_6_1_1.jpg?ts=1649084990703','JACQUARD TIE WITH FISH','2590.00');
+
+tie.push(tie1,tie2,tie3,tie4);
+
+
+//7.
+let bag = [];
+
+let bag1 = new newManData('https://static.zara.net/photos///2022/V/1/1/p/3143/920/500/32/w/590/3143920500_1_1_1.jpg?ts=1650879773571','LEATHER CROSSBODY MAXI BAG','14990.00');
+
+let bag2 = new newManData('https://static.zara.net/photos///2022/V/1/1/p/3143/920/500/33/w/437/3143920500_2_1_1.jpg?ts=1650885927158','LEATHER CROSSBODY MAXI BAG',' 14990.00');
+let bag_2 = new newManData("https://static.zara.net/photos///contents/cm/media-transformations/joinlife-ctx/joinlife-large.svg?ts=1611919362013",'LEATHER CROSSBODY MAXI BAG','14990.00');
+
+let bag3 = new newManData("https://static.zara.net/photos///2022/V/1/1/p/3904/920/002/32/w/590/3904920002_1_1_1.jpg?ts=1650886210615",'EMBOSSED MOBILE PHONE CARRYING CASE','1590.00');
+
+
+//8
+
